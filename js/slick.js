f377fbc816ae9fdc77b81bdf427859ff169b07cb
minhthanh1102 <minhthanh79517@gmail.com>
2022-11-09 15:14:25 +0700

    start

---
+++ js/slick.js
@@ -0,0 +1,35 @@
+$('.banner2').slick({
+    centerMode: true,
+    // centerPadding: '60px',
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: false,
+    infinite: true,
+    // speed: 300,
+    arrows: false,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    // responsive: [
+    //   {
+    //     breakpoint: 768,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: '500px',
+    //       slidesToShow: 1
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: '500px',
+    //       slidesToShow: 1
+    //     }
+    //   }
+    // ]
+  });
+  
